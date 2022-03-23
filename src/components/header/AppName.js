265104e288f099ad2c27c5a90cc00aa1da8e6acf
DongKyu <po4tion0429@gmail.com>
2022-03-23 16:30:41 +0900

    AppName 컴포넌트 env 변수 추가

--- src/components/header/AppName.js
+++ src/components/header/AppName.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-undef */
 import { Center, Text } from '@chakra-ui/react';
 
 function AppName() {
 	return (
 		<Center>
 			<Text fontSize="6xl" fontWeight={700} userSelect="none">
-				COVIG
+				{process.env.REACT_APP_NAME}
 			</Text>
 		</Center>
 	);
