16f88a7375977ceda99d073e0bc9b412dedc4645
Donggyu Kim <po4tion0429@gmail.com>
2022-04-09 19:25:54 +0900

    body redux 적용
    1. redux 적용
    2. 코드 refactoring

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,26 +1,56 @@
+/* eslint-disable no-undef */
+import { useEffect } from 'react';
 import { Topic, OfferingData, CovidCard } from './index';
 import { Box, Flex } from '@chakra-ui/react';
 import { FaVirus } from 'react-icons/fa';
 import { GiDeadWood } from 'react-icons/gi';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCovidBasicInfo } from '../../modules/covid';
+import { calculateDate } from '../../lib';
 
 function Body() {
+	const dispatch = useDispatch();
+	const data = useSelector((state) => state.covid.covidBasicInfo);
+
+	useEffect(() => {
+		dispatch(getCovidBasicInfo());
+	}, []);
+
+	const card = (type) => {
+		const covidData = data[0][calculateDate()];
+
+		if (type === 'decide') {
+			return (
+				<CovidCard
+					title="전체 누적 확진자"
+					total={+covidData.decideCnt}
+					addition={+covidData.DiffDecideCnt}
+					icon={FaVirus}
+				/>
+			);
+		} else {
+			return (
+				<CovidCard
+					title="전체 누적 사망자"
+					total={+covidData.deathCnt}
+					addition={+covidData.DiffDeathCnt}
+					icon={GiDeadWood}
+				/>
+			);
+		}
+	};
+
 	return (
 		<Box border="1px" borderColor="gray.200" m={4}>
 			<Topic />
 			<OfferingData />
 			<Flex justify="space-between">
-				<CovidCard
-					title="전체 누적 확진자"
-					total={7228550}
-					addition={331162}
-					icon={FaVirus}
-				/>
-				<CovidCard
-					title="전체 누적 사망자"
-					total={10800}
-					addition={293}
-					icon={GiDeadWood}
-				/>
+				{data && (
+					<>
+						{card('decide')}
+						{card('death')}
+					</>
+				)}
 			</Flex>
 		</Box>
 	);
