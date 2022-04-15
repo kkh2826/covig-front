170dfd9b2c652d713447382a72f2fbed43207a34
kyounghoon <kkh2826@naver.com>
2022-04-15 21:31:20 +0900

    loading Reducer 생성

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import covid from './covid';
+import loading from './loading';
 
 const rootReducer = combineReducers({
 	covid,
+	loading,
 });
 
 export default rootReducer;
