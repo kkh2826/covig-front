01605a8998893353476386e2e1c5a74d9553de39
Donggyu Kim <po4tion0429@gmail.com>
2022-04-21 22:17:32 +0900

    chart select option 기능 추가

--- src/components/chart/ChartBody.js
+++ src/components/chart/ChartBody.js
@@ -1,17 +1,39 @@
 /* eslint-disable no-unused-vars */
 import { Box, Select } from '@chakra-ui/react';
+import { useState } from 'react';
 import { Line } from 'react-chartjs-2';
 import { options, Datas } from '../../utils';
 
 function ChartBody() {
+	/**
+	 ** default value : 7일
+	 */
+	const [terms, setTerms] = useState(7);
+
+	const handleOptions = (e) => {
+		setTerms(e.target.value);
+	};
+
 	return (
 		<Box mx={4} my={20}>
-			<Select width={'200px'}>
-				<option value="일주일">일주일</option>
-				<option value="일주일">2주</option>
-				<option value="일주일">한달</option>
-			</Select>
-			<Line options={options} data={Datas(30)} />
+			<Box
+				width={'100%'}
+				display={'flex'}
+				alignItems={'center'}
+				justifyContent={'flex-end'}
+			>
+				<Select
+					width={'10rem'}
+					variant="flushed"
+					borderColor={'#000000'}
+					onChange={handleOptions}
+				>
+					<option value="7">일주일</option>
+					<option value="14">2주</option>
+					<option value="30">한달</option>
+				</Select>
+			</Box>
+			<Line options={options} data={Datas(terms)} />
 		</Box>
 	);
 }
