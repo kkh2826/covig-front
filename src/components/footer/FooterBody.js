f41d64f48166f159f21474fb7dd3a229a7c271fc
kyounghoon <kkh2826@naver.com>
2022-05-01 13:27:19 +0900

    Footer Layout 임시 변경

--- src/components/footer/FooterBody.js
+++ src/components/footer/FooterBody.js
@@ -13,65 +13,43 @@ import {
 	Thead,
 	Tr,
 } from '@chakra-ui/react';
+import {
+	Flex,
+	Box,
+	Spacer,
+	Divider,
+	SimpleGrid,
+	Grid,
+	GridItem,
+} from '@chakra-ui/react';
 import { AiFillGithub } from 'react-icons/ai';
 
 function FooterBody() {
 	return (
-		<Center
-			border={'1px solid black'}
-			width={'100vw'}
-			left="0"
-			position={'absolute'}
-		>
-			<Center
-				my={20}
-				p={5}
+		<>
+			<Divider />
+			<Box
+				p={2}
+				m={2}
 				rounded={'lg'}
 				boxShadow={'0px 4px 15px 5px rgba(0, 0, 0, 0.25)'}
 			>
-				<TableContainer>
-					<Table variant="simple">
-						<TableCaption placement="bottom">
-							이 웹사이트는 COVIG 개발진들에 의해 만들어졌습니다.
-						</TableCaption>
-						<Thead>
-							<Tr>
-								<Th>Github Repository</Th>
-								<Th>Email</Th>
-							</Tr>
-						</Thead>
-						<Tbody>
-							<Tr>
-								<Td>
-									<Center>
-										<Link
-											href="https://github.com/po4tion/covig-front"
-											isExternal
-										>
-											<Icon as={AiFillGithub} w={8} h={8} />
-										</Link>
-									</Center>
-								</Td>
-								<Td>po4tion0429@gmail.com</Td>
-							</Tr>
-							<Tr>
-								<Td>
-									<Center>
-										<Link
-											href="https://github.com/kkh2826/COVIG_Back"
-											isExternal
-										>
-											<Icon as={AiFillGithub} w={8} h={8} />
-										</Link>
-									</Center>
-								</Td>
-								<Td>kkh2826@naver.com</Td>
-							</Tr>
-						</Tbody>
-					</Table>
-				</TableContainer>
-			</Center>
-		</Center>
+				<Grid templateColumns="repeat(2, 1fr)">
+					<Center>
+						<Link href="https://github.com/po4tion/covig-front" isExternal>
+							<Icon as={AiFillGithub} w={8} h={8} />
+						</Link>
+						<Text>po4tion0429@gmail.com</Text>
+					</Center>
+					<Center>
+						<Link href="https://github.com/kkh2826/COVIG_Back" isExternal>
+							<Icon as={AiFillGithub} w={8} h={8} />
+						</Link>
+						<Text>kkh2826@naver.com</Text>
+					</Center>
+				</Grid>
+			</Box>
+		</>
 	);
 }
 
