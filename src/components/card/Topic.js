7dafd1da9af24e26a4b9b489b306cce3f9a11267
Donggyu Kim <po4tion0429@gmail.com>
2022-05-01 11:37:22 +0900

    새로고침 버튼 기능 추가

--- src/components/card/Topic.js
+++ src/components/card/Topic.js
@@ -1,13 +1,20 @@
-import { Center, Heading } from '@chakra-ui/react';
+import { Center, Heading, Tooltip } from '@chakra-ui/react';
 import { RepeatIcon } from '@chakra-ui/icons';
+import { useCallback } from 'react';
 
 function Topic() {
+	const refreshBtn = useCallback(() => {
+		window.location.replace('/');
+	}, []);
+
 	return (
 		<Center mb={10}>
 			<Heading as="h2" size="lg">
 				국내 감염 현황
 			</Heading>
-			<RepeatIcon ml={2} />
+			<Tooltip label="새로고침">
+				<RepeatIcon ml={2} w={8} h={8} cursor="pointer" onClick={refreshBtn} />
+			</Tooltip>
 		</Center>
 	);
 }
