92a1bc5c1200bd1bfd9cd960cb7f31e17ae27994
Donggyu Kim <po4tion0429@gmail.com>
2022-05-07 14:46:21 +0900

    test api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import axios from 'axios';
+import { useEffect } from 'react';
 import './App.css';
-import { Main } from './pages';
+// import { Main } from './pages';
+
+// function App() {
+// 	return <Main />;
+// }
 
 function App() {
-	return <Main />;
+	const calling = async () => {
+		await axios
+			.get('https://www.covigback.com/api/covidBasicinfo/')
+			.then((data) => console.log(data));
+	};
+
+	useEffect(async () => {
+		await calling();
+	});
+
+	return <div>값 출력</div>;
 }
 
 export default App;
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,28 +1,14 @@
 /* eslint-disable no-undef */
 
-// export const getCovidBasicInfoFetch = async () => {
-// 	const res = await fetch(`${process.env.REACT_APP_API_URL}/covidBasicinfo/`);
-// 	const data = await res.json();
-
-// 	return data;
-// };
-
-// export const getCovidRegionInfoFetch = async () => {
-// 	const res = await fetch(`${process.env.REACT_APP_API_URL}/covidregioninfo/`);
-// 	const data = await res.json();
-
-// 	return data;
-// };
-
 export const getCovidBasicInfoFetch = async () => {
-	const res = await fetch(`/api/covidBasicinfo/`);
+	const res = await fetch(`${process.env.REACT_APP_API_URL}/covidBasicinfo/`);
 	const data = await res.json();
 
 	return data;
 };
 
 export const getCovidRegionInfoFetch = async () => {
-	const res = await fetch(`/api/covidregioninfo/`);
+	const res = await fetch(`${process.env.REACT_APP_API_URL}/covidregioninfo/`);
 	const data = await res.json();
 
 	return data;
