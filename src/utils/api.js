6fbd207c3c865ec3db06d0e4e41e436a1fd8b90d
Donggyu Kim <po4tion0429@gmail.com>
2022-05-07 14:41:35 +0900

    proxy setting

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,14 +1,28 @@
 /* eslint-disable no-undef */
 
+// export const getCovidBasicInfoFetch = async () => {
+// 	const res = await fetch(`${process.env.REACT_APP_API_URL}/covidBasicinfo/`);
+// 	const data = await res.json();
+
+// 	return data;
+// };
+
+// export const getCovidRegionInfoFetch = async () => {
+// 	const res = await fetch(`${process.env.REACT_APP_API_URL}/covidregioninfo/`);
+// 	const data = await res.json();
+
+// 	return data;
+// };
+
 export const getCovidBasicInfoFetch = async () => {
-	const res = await fetch(`${process.env.REACT_APP_API_URL}/covidBasicinfo/`);
+	const res = await fetch(`/api/covidBasicinfo/`);
 	const data = await res.json();
 
 	return data;
 };
 
 export const getCovidRegionInfoFetch = async () => {
-	const res = await fetch(`${process.env.REACT_APP_API_URL}/covidregioninfo/`);
+	const res = await fetch(`/api/covidregioninfo/`);
 	const data = await res.json();
 
 	return data;
