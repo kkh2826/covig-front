396bfb61d24b1e7a369f68ee56f5a499ae05e79e
Donggyu Kim <po4tion0429@gmail.com>
2022-05-11 13:11:45 +0900

    clg 제거

--- src/components/card/CardBody.js
+++ src/components/card/CardBody.js
@@ -11,8 +11,7 @@ function Body() {
 	const [isLargerThan750] = useMediaQuery('(max-width: 750px)');
 	const dispatch = useDispatch();
 	const data = useSelector((state) => state.covid.covidBasicInfo);
-	console.log('데이터 확인:', data);
-	console.log(calculateDate());
+
 	useEffect(() => {
 		dispatch(getCovidBasicInfo());
 	}, []);
